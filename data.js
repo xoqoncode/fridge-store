c9f5666fced5d97fda301a013fa9ed5623e4b7ca
xoqoncode <210341j@jdu.uz>
2023-02-15 15:54:45 +0500

    yangi loyiha

---
+++ data.js
@@ -0,0 +1,34 @@
+
+    
+    let shopItemsData = [
+        {
+          id: "jfhgbvnscs",
+          name: "MiniFridge,Compact",
+          price: 499,
+          desc: "Refrigeration is an essential food storage ",
+          img: "images/fridge-2.jpg",
+        },
+        {
+          id: "ioytrhndcv",
+          name: "Samsung fridge",
+          price: 669,
+          desc: "221L Top Mount Fridge/Freezer.",
+          img: "images/fridge-1.jpg",
+        },
+        {
+          id: "wuefbncxbsn",
+          name: "LG Refrigerator",
+          price: 559,
+          desc: "LG 375 Top Mount Fridge In Silver",
+          img: "images/fridge-3.jpg",
+        },
+        {
+          id: "thyfhcbcv",
+          name: "22.5 Cu. ft. French",
+          price: 500,
+          desc: "Refrigerator, Deep Freezer, 4 Door",
+          img: "images/fridge-4.jpg",
+        },
+      ];
+      
+
